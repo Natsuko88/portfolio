304ee18e7072e2b1f993e9fcadf58b9a7495b9b5
AyumiYamamura-cloud <138938774+AyumiYamamura-cloud@users.noreply.github.com>
2024-05-18 13:12:20 +0900

    ファイル更新0518

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//swiper
+const swiper = new Swiper(".mySwiper", {
+    //effect: "flip",
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
