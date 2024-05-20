67231c631736c6e2339e0cce76aba0314b9be50f
AyumiYamamura-cloud <138938774+AyumiYamamura-cloud@users.noreply.github.com>
2024-05-20 15:44:04 +0900

    静的コーディング完了0520

--- js/main.js
+++ js/main.js
@@ -10,3 +10,59 @@ const swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+//hbutton
+const hbtn =document.querySelector('.p-hbutton');
+const gnav =document.querySelector('.p-gnav');
+const gnavUl =document.querySelector('.p-gnav-ul');
+
+console.log(hbtn);
+console.log(gnav);
+console.log(gnavUl);
+
+hbtn.addEventListener('click',()=>{
+  hbtn.classList.toggle('is-active');
+  gnav.classList.toggle('is-active');
+  gnavUl.classList.toggle('is-active');
+});
+
+//liクリック時
+gnav.addEventListener('click',()=>{
+  hbtn.classList.remove('is-active');
+  gnav.classList.remove('is-active');
+  gnavUl.classList.remove('is-active');
+});
+
+//リサイズ
+document.addEventListener('DOMContentLoaded', function(){
+  //条件分岐の判断に利用するメディアクエリを指定
+  const mq=window.matchMedia("(min-width:1025px)");
+  //メディアクエリをチェックする関数を定義して実行
+  function mqCheck(mq){
+      //if PCサイズ（１０２５px以上の場合）
+      if(mq.matches){
+          hbtn.classList.remove('is-active');
+          gnav.classList.remove('is-active');
+          gnavUl.classList.remove('is-active');
+      }else{
+          //else tabサイズ以下（１０２４px以下の場合）
+      }  
+  }
+  mqCheck(mq);
+  // MediaQueryListが持つ情報が変更された場合に、定義した関数を実行
+  mq.addEventListener('change', mqCheck, false);
+}, false);
+
+//tophead animation
+const title= document.querySelector('.p-site-title');
+
+const keyframes = {
+  opacity:[0,0.8],
+  translate:['0 50px',0],
+};
+const options = {
+  duration:2000,
+  easing:'ease',
+};
+
+title.animate(keyframes,options);
