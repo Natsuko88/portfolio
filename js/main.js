9470ad8e289427614a96d4f4f0dbe2ddf1127e92
AyumiYamamura-cloud <138938774+AyumiYamamura-cloud@users.noreply.github.com>
2024-05-21 10:25:51 +0900

    静的コーディング完了0521

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
 //swiper
-const swiper = new Swiper(".mySwiper", {
-    //effect: "flip",
-    grabCursor: true,
-    pagination: {
-      el: ".swiper-pagination",
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
     },
-  });
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
 
 //hbutton
 const hbtn =document.querySelector('.p-hbutton');
